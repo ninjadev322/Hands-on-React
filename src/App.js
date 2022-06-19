bd619e71133ce0ce6f4b2c213fff21eb433d832b
ninjadev322 <kotovubalenko.ku@gmail.com>
2022-06-19 13:18:57 -1000

    Your First Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
 
 function App() {
   return (
-    <blockquote cite="Benjamin Franklin">
-      Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
+    // <blockquote cite="Benjamin Franklin">
+    //   Tell me and I forget, teach me and I may remember, involve me and I learn.
+    // </blockquote>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
@@ -23,6 +24,9 @@ function App() {
     //     </a>
     //   </header>
     // </div>
+    <div className="container">
+      <ProjectsPage/>
+    </div>
   );
 }
 
