c16ca11f41dc7d2b1f9c0edc464f56386122efa1
ninjadev322 <kotovubalenko.ku@gmail.com>
2022-06-21 14:39:12 -1000

    Reusable Components

--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Project } from './Project';
+import ProjectCard from './ProjectCard';
 
 ProjectList.propTypes = {
     projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired
@@ -11,20 +12,15 @@ function ProjectList({ projects }) {
         <div className="row">
             {projects.map((project) => (
                 <div key={project.id} className="cols-sm">
-                    <div className='card'>
-                        <img src={project.imageUrl} alt={project.name} />
-                        <section className='section dark'>
-                            <h5 className='strong'>
-                                <strong>{project.name}</strong>
-                            </h5>
-                            <p>{project.description}</p>
-                            <p>Budget : {project.budget.toLocaleString()}</p>
-                        </section>
-                    </div>
+                    <ProjectCard project={project}></ProjectCard>
                 </div>
             ))}
         </div>
     );
 }
+
+ProjectList.propTypes = {
+    project: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired,
+};
 
 export default ProjectList;
